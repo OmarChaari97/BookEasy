7e63bca93959313e332bd39c2991511a556a277c
OmarChaari97 <omar.chaari97@gmail.com>
2023-05-29 20:47:45 +0000

    Complete admin

--- API/routes/hotels.js
+++ API/routes/hotels.js
@@ -1,13 +1,14 @@
 import express from "express";
 import hotel from "../models/hotel.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
 import {createHotel, updateHotel,deleteHotel,getHotel,getHotels} from "../controllers/hotelController.js"
 const router = express.Router();
 //Create hotel
-router.post("/createHotel", createHotel)
+router.post("/createHotel",verifyAdmin, createHotel)
 //Update hotel
-router.put("/updateHotel/:id",updateHotel)
+router.put("/updateHotel/:id",verifyAdmin,updateHotel)
 // Delete  hotele
-router.delete("/deleteHotel/:id",deleteHotel)
+router.delete("/deleteHotel/:id",verifyAdmin,deleteHotel)
 // Find hotel by id
 router.get("/:id",getHotel)
 // Find All hotel
--- API/routes/users.js
+++ API/routes/users.js
@@ -1,15 +1,22 @@
 import express from "express";
 import {updateUser, deleteUser, getUser, getUsers} from "../controllers/userController.js"
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser,verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 router.get("/checkAuthentication",verifyToken, (req,res,next)=>{
     res.send("Heloo user !!!!")
 })
-router.put("/updateUser/:id",updateUser)
+router.get("/checkUser/:id",verifyUser, (req,res,next)=>{
+    res.send("Heloo user, you are logged and u can delete your account !!!!")
+})
+router.get("/checkAdmin/:id",verifyAdmin, (req,res,next)=>{
+    res.send("Heloo admin, you are logged and u can delete all accounts !!!!")
+})
+
+router.put("/updateUser/:id",verifyUser,updateUser)
 // Delete  user
-router.delete("/deleteUser/:id",deleteUser)
+router.delete("/deleteUser/:id",verifyUser,deleteUser)
 // Find user by id
-router.get("/:id",getUser)
+router.get("/:id",verifyUser,getUser)
 // Find All users
-router.get("/",getUsers)
+router.get("/",verifyAdmin,getUsers)
 export default router
